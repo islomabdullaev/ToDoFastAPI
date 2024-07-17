49d06c507a8495b67ed233dfd722e512d6e6437b
islomabdullaev <islom.abdullaev.02@mail.ru>
2024-07-17 18:08:03 +0500

    crud with details

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import models
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path, status, Response
 from database import engine
 from sqlalchemy.orm import Session
 from general import get_session
-from schemas import AssignmentCreateSchema
+from schemas import AssignmentCreateSchema, AssignmentEditSchema, AssignmentPriorityEditSchema
+
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -11,14 +12,12 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI(title="To Do Project")
 
 
-@app.get('/assignments')
+@app.get('/assignments', status_code=status.HTTP_200_OK)
 async def get_assignments(session: Session = Depends(get_session)):
     assignments = session.query(models.AssignmentTable).all()
-    return {
-        "assignments": assignments
-    }
+    return assignments
 
-@app.post('/assignments')
+@app.post('/assignments', status_code=status.HTTP_201_CREATED)
 async def create_assignment(
     data: AssignmentCreateSchema,
     session: Session = Depends(get_session)):
@@ -33,3 +32,61 @@ async def create_assignment(
     session.refresh(assignment)
 
     return assignment
+
+
+@app.get('/assignments/{assignment_id}', status_code=status.HTTP_200_OK)
+async def get_assignment(
+        assignment_id: int = Path(gt=0),
+        session: Session = Depends(get_session)):
+    assignment = session.query(models.AssignmentTable).filter(
+        models.AssignmentTable.id == assignment_id).first()
+    if assignment is not None:
+        return assignment
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found !")
+
+
+@app.put('/assignments/{assignment_id}', status_code=status.HTTP_201_CREATED)
+async def update_assignment(
+    data: AssignmentEditSchema,
+    assignment_id: int = Path(gt=0),
+    session: Session = Depends(get_session)):
+    assignment = session.query(models.AssignmentTable).filter(
+        models.AssignmentTable.id == assignment_id).first()
+    if assignment is not None:
+        assignment.title = data.title
+        assignment.description = data.description
+        assignment.is_completed = data.is_completed
+        session.add(assignment)
+        session.commit()
+        session.refresh(assignment)
+        return assignment
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found !")
+
+
+@app.patch('/assignments/{assignment_id}', status_code=status.HTTP_201_CREATED)
+async def update_assignment(
+    data: AssignmentPriorityEditSchema,
+    assignment_id: int = Path(gt=0),
+    session: Session = Depends(get_session)):
+    assignment = session.query(models.AssignmentTable).filter(
+        models.AssignmentTable.id == assignment_id).first()
+    if assignment is not None:
+        assignment.priority = data.priority
+        session.add(assignment)
+        session.commit()
+        session.refresh(assignment)
+        return assignment
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found !")
+
+@app.delete('/assignments/{assignment_id}', status_code=status.HTTP_204_NO_CONTENT)
+async def delete_assignment(
+    assignment_id: int = Path(gt=0),
+    session: Session = Depends(get_session)
+    ):
+    assignment = session.query(models.AssignmentTable).filter(
+        models.AssignmentTable.id == assignment_id).first()
+    if assignment is not None:
+        session.delete(assignment)
+        session.commit()
+        return Response(content="Deleted!", status_code=status.HTTP_204_NO_CONTENT)
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Not found !")
--- schemas.py
+++ schemas.py
@@ -7,4 +7,13 @@ class AssignmentCreateSchema(BaseModel):
     description: str
     priority: str
     created_at: datetime = datetime.now(tashkent_tz)
-    
+
+
+class AssignmentEditSchema(BaseModel):
+    title: str | None = None
+    description: str | None = None
+    is_completed: bool
+
+
+class AssignmentPriorityEditSchema(BaseModel):
+    priority: str
